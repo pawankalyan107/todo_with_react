4a83938247ab8acb5dcdc61766d1b7ae717c4258
Pawankalyan Veldurthi <Pawan@blrgle.in>
2022-04-26 10:04:44 +0530

    made components in seperate files and used props

--- src/Input.js
+++ src/Input.js
@@ -1,62 +1,60 @@
 import React from "react";
-import Tasks from "./Tasks"
+import Tasks from "./Tasks";
 
 class Input extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoading: true,
-        todo: [],
-      };
-    }
-  
-    render() {
-  
-          async function fetchJSON() {
-              const response = await fetch('https://api.github.com/users?since=135');
-              const data = await response.json();
-              return data;
-          }
-          if (this.state.isLoading){
-        fetchJSON().then(res=>{
-          res.map((ele)=>{
-            return this.setState({
-              ...this.setState,
-              todo: [...this.state.todo, ele.login]
-            })
-          })
-        })
-        this.setState({
-          ...this.state,
-          isLoading:false
-        })
-          }
-  
-      const add_items = () => {
-  
-        const input_value = document.querySelector(".inputField").value;
-        this.setState({
-          ...this.state,
-          todo: [...this.state.todo, input_value],
-        });
-        document.querySelector(".inputField").value = "";
-      };
-      return (
-        <>
-          <input
-            className="inputField"
-            id="new-task-input"
-            placeholder="Write the task ToDo"
-            type={"text"}
-          ></input>
-          <button className="new-task-submit" onClick={add_items}>
-            Add
-          </button>
-          <br></br>
-          <Tasks text={this.state.todo} />
-        </>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      todo: [],
+    };
   }
 
-export default Input
+  render() {
+    async function fetchJSON() {
+      const response = await fetch("https://api.github.com/users?since=135");
+      const data = await response.json();
+      return data;
+    }
+    if (this.state.isLoading) {
+      fetchJSON().then((res) => {
+        res.map((ele) => {
+          return this.setState({
+            ...this.setState,
+            todo: [...this.state.todo, ele.login],
+          });
+        });
+      });
+      this.setState({
+        ...this.state,
+        isLoading: false,
+      });
+    }
+
+    const add_items = () => {
+      const input_value = document.querySelector(".inputField").value;
+      this.setState({
+        ...this.state,
+        todo: [...this.state.todo, input_value],
+      });
+      document.querySelector(".inputField").value = "";
+    };
+    return (
+      <>
+        <input
+          className="inputField"
+          id="new-task-input"
+          placeholder="Write the task ToDo"
+          type={"text"}
+        ></input>
+        <button className="new-task-submit" onClick={add_items}>
+          Add
+        </button>
+        <br></br>
+        <Tasks text={this.state.todo} />
+      </>
+    );
+  }
+}
+
+export default Input;
